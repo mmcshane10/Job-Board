aca54f177dd4c76dcf2bca5a9abe39bad593ec7f
Devin Cooley and Mike McShane <devin+mike@>
2019-09-12 10:07:47 -0700

    Add second promise and place promises in promis.all function

--- src/main.js
+++ src/main.js
@@ -12,39 +12,39 @@ $(document).ready(function() {
     let location = $('#location-keyword').val();
     $('#job-keyword').val("");
     $('#location-keyword').val("");
-    $('.populate').text("")
+    $('.populate').text("");
 
-    let jobSearch = new JobSearch();
-    let promise = jobSearch.getJobPosting(location, keyword);
+    let ghjobSearch = new JobSearch();
+    let promise1 = ghjobSearch.getGHJobPosting(location, keyword);
+    let ajjobsearch = new JobSearch();
+    let promise2 = ajjobsearch.getAJJobPosting(location, keyword);
 
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      for (var i = 0; i < body.length; i++) {
+    Promise.all([promise1,promise2]).then(function(response) {
+      console.log(response);
+      let bodyGH = JSON.parse(response[0]);
+      for (var i = 0; i < bodyGH.length; i++) {
         $(".populate").append(
           `<div class="card bg-light mb-3">
             <div class="card-header">
               <div id=title${i}></div>
             </div>
-            <div class="card-body">
+            <div class="card-bodyGH">
               <div id=company${i}></div>
               <div id=location${i}></div>
               <div id=description${i}></div>
               <div id=url${i}></div>
             </div>
           </div>`);
-        // $(`#logo${i}`).html(`<img src=${body[i].company_logo}>`);
-        $(`#title${i}`).html(body[i].title);
-        $(`#company${i}`).html(
-
-          `<p> <span class='strong'>Company:</span> <a href=${body[i].company_url}> ${body[i].company} </a> </p>`
-
-
-        );
-        $(`#location${i}`).html(`<p><span class='strong'>Location:</span> ${body[i].location}</p>`);
-        $(`#url${i}`).html(`<p><span class='strong'><a href=${body[i].url}>Visit Website</a></span></p>`)
-        $(`#description${i}`).html(body[i].description);
+        // $(`#logo${i}`).html(`<img src=${bodyGH[i].company_logo}>`);
+        $(`#title${i}`).html(bodyGH[i].title);
+        $(`#company${i}`).html(`<p><span class='strong'>Company:</span> <a href=${bodyGH[i].company_url}>${bodyGH[i].company}</a></p>`);
+        $(`#location${i}`).html(`<p><span class='strong'>Location:</span> ${bodyGH[i].location}</p>`);
+        $(`#url${i}`).html(`<p><span class='strong'><a href=${bodyGH[i].url}>Visit Website</a></span></p>`);
+        $(`#description${i}`).html(bodyGH[i].description);
       }
-      console.log(body);
+      let bodyAJ = JSON.parse(response[1]);
+      console.log(bodyGH);
+      console.log(bodyAJ);
     }, function (error) {
       $('#showErrors').text(`There was an error processing your request: ${error.message}`);
     });
